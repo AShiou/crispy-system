9edb5f12a2de2e02c7ea20c554556cf753215509
AShiou <aa8693@gmail.com>
2020-07-15 23:07:30 +0800

    add HomeController test

--- src/test/java/com/shiou/crispysystem/HomeControllerTest.java
+++ src/test/java/com/shiou/crispysystem/HomeControllerTest.java
@@ -1,6 +1,17 @@
 package com.shiou.crispysystem;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 /**
  * TODO
@@ -9,8 +20,19 @@ import static org.junit.jupiter.api.Assertions.*;
  * @version 1.0.0
  * @since 2020/07/15 18:17
  */
-@RunWith (SpringRunner.class)
-@WebMvcTest (HomeController.class)
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
 class HomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(
+                        containsString("Welcome to...")));
+    }
 
 }
