4a1e3c0055481cc085236fb7ef4e9164df90e59d
AShiou <aa8693@gmail.com>
2020-07-15 18:51:25 +0800

    add HomeController

---
+++ src/test/java/com/shiou/crispysystem/HomeControllerTest.java
@@ -0,0 +1,16 @@
+package com.shiou.crispysystem;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * TODO
+ *
+ * @author shiou
+ * @version 1.0.0
+ * @since 2020/07/15 18:17
+ */
+@RunWith (SpringRunner.class)
+@WebMvcTest (HomeController.class)
+class HomeControllerTest {
+
+}
