ecd046fbe108304b8cfb3ef3b2572941a73b9141
AShiou <aa8693@gmail.com>
2020-07-21 18:40:14 +0800

    Ch2 form

--- src/main/java/com/shiou/crispysystem/DecorateCakeController.java
+++ src/main/java/com/shiou/crispysystem/DecorateCakeController.java
@@ -1,8 +1,11 @@
 package com.shiou.crispysystem;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.shiou.crispysystem.Decoration.Type;
 
@@ -10,7 +13,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 @Slf4j
+@Controller
 @RequestMapping("/decorate")
 public class DecorateCakeController {
 
@@ -21,6 +27,7 @@ public class DecorateCakeController {
                 new Decoration("CRCH", "Chocolate Cream", Type.CREAM),
                 new Decoration("FRST", "Strawberry", Type.FRUIT),
                 new Decoration("FRAP", "Apple", Type.FRUIT),
+                new Decoration("FRBL", "Blueberry", Type.FRUIT),
                 new Decoration("CYFO", "Fondant", Type.CANDY),
                 new Decoration("CYCH", "Chocolate", Type.CANDY),
                 new Decoration("CEQU", "Question Mark", Type.CANDLE),
@@ -35,10 +42,19 @@ public class DecorateCakeController {
         return "decorate";
     }
 
+    @PostMapping
+    public String processDecorate(@Valid Cake decorate, Errors errors) {
+        if (errors.hasErrors()) {
+            return "decorate";
+        }
+        log.info("Processing decorate: " + decorate);
+        return "redirect:/orders/current";
+    }
+
     private List<Decoration> filterByType(List<Decoration> decorations, Type type) {
         return decorations
                 .stream()
-                .filter(x -> x.getType().equals(Type))
-                .collect(Collectors.toList(Collectors.toList());
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
     }
 }
